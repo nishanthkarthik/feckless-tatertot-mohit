231e76e4df3f21de7cf184e666797ebc964e359f
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-08-20 14:33:10 +0530

    Refactored Variables, separated declaration and assignment

--- HeatExchange/HeatExchange/SciHelper.cs
+++ HeatExchange/HeatExchange/SciHelper.cs
@@ -172,6 +172,47 @@ namespace HeatExchange
             double TAirFinal;
             double TWaterMeanFinal;
             double TAirMeanFinal;
+            double ViscosityAir;
+            double ViscosityWater;
+            double CpAir;
+            double CpWater;
+            double PrandtlAir;
+            double PrandtlWater;
+            double G_Air;
+            double G_Water;
+            double Reynolds_Air;
+            double Reynolds_Water;
+            double J_Water;
+            double F_Water;
+            double J_Air;
+            double F_Air;
+            double H_Water;
+            double H_Air;
+            double c7;
+            double c8;
+            double c9;
+            double VelocityAir;
+            double FinEfficiency;
+            double OverallEfficiency;
+            double Rw;
+            double UaInverse;
+            double C_Water;
+            double C_Air;
+            double C_Min;
+            double C_Star;
+            double NTU;
+            double epsilon;
+            double ConductionAreaWater;
+            double ConductionAreaAir;
+            double lambdaAir;
+            double lambdaWater;
+            double M1;
+            double M2;
+            double EpsilonRatio;
+            double deltaEpsilon;
+            double ActualEpsilon;
+            double HeatTransferRate;
+            double prevval;
             #endregion
 
             #region User Input acquisition
@@ -202,7 +243,9 @@ namespace HeatExchange
 
             MassFlowWaterFinal = QWaterInitial * DensityWaterFinal;
             MassFlowAirFinal = QAirInitial * DensityAirFinal;
+            #endregion
 
+            #region TMean calculation
             TWaterFinal = TWaterInitial - Epsilon * (TWaterInitial - TAirInitial);
             TAirFinal = TAirInitial + Epsilon * (MassFlowWaterFinal / MassFlowAirFinal) * (TWaterInitial - TAirInitial);
             TWaterMeanFinal = (TWaterFinal + TWaterInitial) / 2;
@@ -211,37 +254,33 @@ namespace HeatExchange
 
             #region CoolProp
             //Calculate additional properties
-            double ViscosityAir = CoolProp.PropsSI("V", "T", TAirMeanFinal, "P", 101325, "Air");
-            double ViscosityWater = CoolProp.PropsSI("V", "T", TWaterMeanFinal, "P", 101325, "Water");
-            double CpAir = CoolProp.PropsSI("CPMASS", "T", TWaterMeanFinal, "P", 101325, "Water");
-            double CpWater = CoolProp.PropsSI("CPMASS", "T", TWaterMeanFinal, "P", 101325, "Water");
-            double PrandtlAir = CoolProp.PropsSI("CPMASS", "T", TWaterMeanFinal, "P", 101325, "Water");
-            double PrandtlWater = CoolProp.PropsSI("CPMASS", "T", TWaterMeanFinal, "P", 101325, "Water");
+            ViscosityAir = CoolProp.PropsSI("V", "T", TAirMeanFinal, "P", 101325, "Air");
+            ViscosityWater = CoolProp.PropsSI("V", "T", TWaterMeanFinal, "P", 101325, "Water");
+            CpAir = CoolProp.PropsSI("CPMASS", "T", TWaterMeanFinal, "P", 101325, "Water");
+            CpWater = CoolProp.PropsSI("CPMASS", "T", TWaterMeanFinal, "P", 101325, "Water");
+            PrandtlAir = CoolProp.PropsSI("CPMASS", "T", TWaterMeanFinal, "P", 101325, "Water");
+            PrandtlWater = CoolProp.PropsSI("CPMASS", "T", TWaterMeanFinal, "P", 101325, "Water");
             #endregion
 
             #region Calculate G and Re
-            double G_Air = MassFlowAirFinal /
-                              ((_in.SysType == SystemType.Inline)
-                                  ? _in.TubeOutsideInlineData.TotalMinFfArea
-                                  : _in.TubeOutsideStaggered.TotalMinFfArea);
+            G_Air = MassFlowAirFinal /
+                             ((_in.SysType == SystemType.Inline)
+                                 ? _in.TubeOutsideInlineData.TotalMinFfArea
+                                 : _in.TubeOutsideStaggered.TotalMinFfArea);
 
-            double G_Water = MassFlowWaterFinal /
-                                          ((_in.SysType == SystemType.Inline)
-                                              ? _in.TubeOutsideInlineData.TotalMinFfArea
-                                              : _in.TubeOutsideStaggered.TotalMinFfArea);
+            G_Water = MassFlowWaterFinal /
+                                         ((_in.SysType == SystemType.Inline)
+                                             ? _in.TubeOutsideInlineData.TotalMinFfArea
+                                             : _in.TubeOutsideStaggered.TotalMinFfArea);
 
-            double Reynolds_Air = G_Air * ((_in.SysType == SystemType.Inline)
-                ? _in.TubeOutsideInlineData.HydDiameter
-                : _in.TubeOutsideStaggered.HydDiameter) / ViscosityAir;
+            Reynolds_Air = G_Air * ((_in.SysType == SystemType.Inline)
+               ? _in.TubeOutsideInlineData.HydDiameter
+               : _in.TubeOutsideStaggered.HydDiameter) / ViscosityAir;
 
-            double Reynolds_Water = G_Water * ((_in.SysType == SystemType.Inline)
-                ? _in.TubeOutsideInlineData.HydDiameter
-                : _in.TubeOutsideStaggered.HydDiameter) / ViscosityWater;
+            Reynolds_Water = G_Water * ((_in.SysType == SystemType.Inline)
+               ? _in.TubeOutsideInlineData.HydDiameter
+               : _in.TubeOutsideStaggered.HydDiameter) / ViscosityWater;
 
-            double J_Water;
-            double F_Water;
-            double J_Air;
-            double F_Air;
             #endregion
 
             #region Calculation of J
@@ -316,14 +355,14 @@ namespace HeatExchange
             #endregion
 
             #region H Calculation
-            double H_Water = J_Water * G_Water * CpWater / Math.Pow(PrandtlWater, (double)2 / 3);
-            double H_Air = J_Air * G_Air * CpAir / Math.Pow(PrandtlAir, (double)2 / 3);
-            #endregion  
+            H_Water = J_Water * G_Water * CpWater / Math.Pow(PrandtlWater, (double)2 / 3);
+            H_Air = J_Air * G_Air * CpAir / Math.Pow(PrandtlAir, (double)2 / 3);
+            #endregion
 
             #region F Calculation
-            double c7 = -0.764 + 0.739 * Xt / Xl + 0.177 * FinPitch / Do - 0.00758 / Nr;
-            double c8 = -15.689 + 64.021 / Math.Log(Reynolds_Water);
-            double c9 = 1.696 - 15.695 / Math.Log(Reynolds_Water);
+            c7 = -0.764 + 0.739 * Xt / Xl + 0.177 * FinPitch / Do - 0.00758 / Nr;
+            c8 = -15.689 + 64.021 / Math.Log(Reynolds_Water);
+            c9 = 1.696 - 15.695 / Math.Log(Reynolds_Water);
             F_Water = 0.0267 * Math.Pow(Reynolds_Water, c7) * Math.Pow(Xt / Xl, c8) * Math.Pow(FinPitch / Do, c9);
 
             c7 = -0.764 + 0.739 * Xt / Xl + 0.177 * FinPitch / Do - 0.00758 / Nr;
@@ -334,26 +373,26 @@ namespace HeatExchange
 
             #region Fin Efficiency
 
-            double VelocityAir = QAirInitial / (L1 * L3 - Nr * L1 * Do);
+            VelocityAir = QAirInitial / (L1 * L3 - Nr * L1 * Do);
             if (VelocityAir <= 15 && VelocityAir >= 6)
             {
                 MessageBox.Show("Velocity should be between 6m/s and 15m/s", "Input out of limits");
                 return;
             }
 
-            double FinEfficiency = 7.41 * Math.Pow(VelocityAir, -0.12) *
-                                   Math.Pow(Xt / (2 * FinThickness), -2.32) * Math.Pow(Xl / (2 * FinThickness), -0.198);
+            FinEfficiency = 7.41 * Math.Pow(VelocityAir, -0.12) *
+                                  Math.Pow(Xt / (2 * FinThickness), -2.32) * Math.Pow(Xl / (2 * FinThickness), -0.198);
 
-            double OverallEfficiency = 1 - ((1 - FinEfficiency) * (L2 * L3 - _in.Nt * Math.PI * Do * Do) /
-                ((_in.SysType == SystemType.Inline) ? _in.TubeOutsideInlineData.TotalHeatArea : _in.TubeOutsideStaggered.TotalHeatArea));
+            OverallEfficiency = 1 - ((1 - FinEfficiency) * (L2 * L3 - _in.Nt * Math.PI * Do * Do) /
+               ((_in.SysType == SystemType.Inline) ? _in.TubeOutsideInlineData.TotalHeatArea : _in.TubeOutsideStaggered.TotalHeatArea));
 
-            double Rw = (Do - Di) / (_in.MaterialCoeff * (_in.Nt + 1) * 2 * L1 * L2);
+            Rw = (Do - Di) / (_in.MaterialCoeff * (_in.Nt + 1) * 2 * L1 * L2);
 
             #endregion
 
             #region UA Calculation
 
-            double UaInverse;
+
             if (_in.SysType == SystemType.Inline)
                 UaInverse = Rw + (1 / (OverallEfficiency * H_Water * _in.TubeOutsideInlineData.TotalHeatArea)) +
                             (1 / (OverallEfficiency * H_Air * _in.TubeOutsideInlineData.TotalHeatArea));
@@ -361,28 +400,28 @@ namespace HeatExchange
                 UaInverse = Rw + (1 / (OverallEfficiency * H_Water * _in.TubeOutsideStaggered.TotalHeatArea)) +
                             (1 / (OverallEfficiency * H_Air * _in.TubeOutsideStaggered.TotalHeatArea));
 
-            double C_Water = MassFlowWaterFinal * CpWater;
-            double C_Air = MassFlowAirFinal * CpAir;
-            double C_Min = Math.Min(C_Air, C_Water);
-            double C_Star = (C_Water / C_Air);
+            C_Water = MassFlowWaterFinal * CpWater;
+            C_Air = MassFlowAirFinal * CpAir;
+            C_Min = Math.Min(C_Air, C_Water);
+            C_Star = (C_Water / C_Air);
             if (C_Star > 1) C_Star = 1 / C_Star;
 
-            double NTU = 1 / (UaInverse * C_Min);
+            NTU = 1 / (UaInverse * C_Min);
 
             #endregion
 
-            double epsilon = FactorialCompute(25, NTU, C_Star);
-            double ConductionAreaWater = _in.Nt * Math.PI * Di * L1;
-            double ConductionAreaAir = _in.Nt * Math.PI * Do * L1;
+            epsilon = FactorialCompute(25, NTU, C_Star);
+            ConductionAreaWater = _in.Nt * Math.PI * Di * L1;
+            ConductionAreaAir = _in.Nt * Math.PI * Do * L1;
 
-            double lambdaAir = _in.MaterialCoeff * ConductionAreaAir / (L1 * C_Air);
-            double lambdaWater = _in.MaterialCoeff * ConductionAreaWater / (L1 * C_Water);
+            lambdaAir = _in.MaterialCoeff * ConductionAreaAir / (L1 * C_Air);
+            lambdaWater = _in.MaterialCoeff * ConductionAreaWater / (L1 * C_Water);
 
-            double M1 = H_Air / H_Water;
-            double M2 = lambdaWater / lambdaAir;
+            M1 = H_Air / H_Water;
+            M2 = lambdaWater / lambdaAir;
 
             XPlatHelper.OpenImageTables();
-            double EpsilonRatio;
+
 
             while (true)
             {
@@ -399,16 +438,16 @@ namespace HeatExchange
                 }
             }
 
-            double deltaEpsilon = EpsilonRatio * Epsilon;
-            double ActualEpsilon = Epsilon - deltaEpsilon;
+            deltaEpsilon = EpsilonRatio * Epsilon;
+            ActualEpsilon = Epsilon - deltaEpsilon;
 
-            double HeatTransferRate = ActualEpsilon * (TAirInitial - TWaterInitial) * C_Min;
+            HeatTransferRate = ActualEpsilon * (TAirInitial - TWaterInitial) * C_Min;
 
             //Air
-            double prevval = TAirFinal;
+            prevval = TAirFinal;
             do
             {
-                TAirFinal = prevval - HeatTransferRate/C_Air;
+                TAirFinal = prevval - HeatTransferRate / C_Air;
             } while (true);
 
         }
